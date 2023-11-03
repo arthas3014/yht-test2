7e130c13cb9f133349a13136a028f7dede4aa6dd
arthas3014 <yht3014@gmail.com>
2023-11-03 17:36:12 +0800

    update

---
+++ PrintExample.java
@@ -0,0 +1,9 @@
+public class PrintExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int i = 1;
+        i++;
+        System.err.println(i);
+        System.out.println("This is a Java print example.");
+    }
+}
